98179665d645d967ab401ef4a02d8e786b6f7fe5
diasalpysbayev <alpysbayevdias01@gmail.com>
2024-07-18 22:11:24 +0500

    microfrontends

---
+++ frontend/microfrontend/footer/src/root.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css"
+
+export default function Root(props) {
+  return <footer className="footer page__section">
+    <p className="footer__copyright">
+      © 2021 Mesto Russia
+    </p>
+  </footer>;
+}
