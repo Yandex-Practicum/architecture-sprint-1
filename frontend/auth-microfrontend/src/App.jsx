1accfdd8cdd6868aa0ff12e39c80dea434b724e6
baydinvladislav <baydinvladislav@yandex.ru>
2024-08-02 20:07:35 +0300

    import auth api service

--- frontend/auth-microfrontend/src/App.jsx
+++ frontend/auth-microfrontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import Register from "./components/Register";
 import InfoTooltip from "./components/InfoTooltip";
 
--- frontend/auth-microfrontend/webpack.config.js
+++ frontend/auth-microfrontend/webpack.config.js
@@ -82,6 +82,7 @@ module.exports = (_, argv) => ({
         "./Login": "./src/components/Login",
         "./Register": "./src/components/Register",
         "./InfoTooltip": "./src/components/InfoTooltip",
+        "./ApiAuthService": "./src/api/AuthService",
       },
       shared: {
         ...deps,
