70c14151fbf04aa04156d9a8b53175cf81e2f5cc
aadanilov <danilow.aleksei@gmail.com>
2024-08-08 13:24:27 +0300

    TASK-0. Довел приложение до работоспособности

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -111,7 +111,7 @@ function App() {
   }
 
   function handleCardLike(card) {
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((id) => id === currentUser._id);
     api
       .changeLikeCardStatus(card._id, !isLiked)
       .then((newCard) => {
