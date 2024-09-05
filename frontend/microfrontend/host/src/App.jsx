8aa57ccc1bca2d0159eb0d2e6c43d3faf721afcd
Дмитрий Соколов <DmiSokolov@x5.ru>
2024-09-05 23:45:51 +0300

    задание 1 + задание 2

---
+++ frontend/microfrontend/host/src/App.jsx
@@ -0,0 +1,284 @@
+import React from "react";
+import { Route, useHistory, Switch } from "react-router-dom";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+
+const PopupWithForm = lazy(() =>
+  import("auth/PopupWithForm").catch(() => {
+    return {
+      default: () => <div className="error">Component is not available!</div>,
+    };
+  }),
+);
+const Register = lazy(() =>
+  import("auth/Register").catch(() => {
+    return {
+      default: () => <div className="error">Component is not available!</div>,
+    };
+  }),
+);
+const Login = lazy(() =>
+  import("auth/Login").catch(() => {
+    return {
+      default: () => <div className="error">Component is not available!</div>,
+    };
+  }),
+);
+const ImagePopup = lazy(() =>
+  import("photos/ImagePopup").catch(() => {
+    return {
+      default: () => <div className="error">Component is not available!</div>,
+    };
+  }),
+);
+const AddPlacePopup = lazy(() =>
+  import("photos/AddPlacePopup").catch(() => {
+    return {
+      default: () => <div className="error">Component is not available!</div>,
+    };
+  }),
+);
+const InfoTooltip = lazy(() =>
+  import("photos/InfoTooltip").catch(() => {
+    return {
+      default: () => <div className="error">Component is not available!</div>,
+    };
+  }),
+);
+const EditProfilePopup = lazy(() =>
+  import("profile/EditProfilePopup").catch(() => {
+    return {
+      default: () => <div className="error">Component is not available!</div>,
+    };
+  }),
+);
+const EditAvatarPopup = lazy(() =>
+  import("profile/EditAvatarPopup").catch(() => {
+    return {
+      default: () => <div className="error">Component is not available!</div>,
+    };
+  }),
+);
+
+import ProtectedRoute from "./ProtectedRoute";
+import api from "../utils/api";
+import * as auth from "../utils/auth.js";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+
+function App() {
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] =
+    React.useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] =
+    React.useState(false);
+  const [selectedCard, setSelectedCard] = React.useState(null);
+  const [cards, setCards] = React.useState([]);
+
+  // В корневом компоненте App создана стейт-переменная currentUser. Она используется в качестве значения для провайдера контекста.
+  const [currentUser, setCurrentUser] = React.useState({});
+
+  const [isInfoToolTipOpen, setIsInfoToolTipOpen] = React.useState(false);
+  const [tooltipStatus, setTooltipStatus] = React.useState("");
+
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  //В компоненты добавлены новые стейт-переменные: email — в компонент App
+  const [email, setEmail] = React.useState("");
+
+  const history = useHistory();
+
+  // Запрос к API за информацией о пользователе и массиве карточек выполняется единожды, при монтировании.
+  React.useEffect(() => {
+    api
+      .getAppInfo()
+      .then(([cardData, userData]) => {
+        setCurrentUser(userData);
+        setCards(cardData);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  // при монтировании App описан эффект, проверяющий наличие токена и его валидности
+  React.useEffect(() => {
+    const token = localStorage.getItem("jwt");
+    if (token) {
+      auth
+        .checkToken(token)
+        .then((res) => {
+          setEmail(res.data.email);
+          setIsLoggedIn(true);
+          history.push("/");
+        })
+        .catch((err) => {
+          localStorage.removeItem("jwt");
+          console.log(err);
+        });
+    }
+  }, [history]);
+
+  function handleEditProfileClick() {
+    setIsEditProfilePopupOpen(true);
+  }
+
+  function handleAddPlaceClick() {
+    setIsAddPlacePopupOpen(true);
+  }
+
+  function handleEditAvatarClick() {
+    setIsEditAvatarPopupOpen(true);
+  }
+
+  function closeAllPopups() {
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setIsEditAvatarPopupOpen(false);
+    setIsInfoToolTipOpen(false);
+    setSelectedCard(null);
+  }
+
+  function handleCardClick(card) {
+    setSelectedCard(card);
+  }
+
+  function handleUpdateUser(userUpdate) {
+    api
+      .setUserInfo(userUpdate)
+      .then((newUserData) => {
+        setCurrentUser(newUserData);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleUpdateAvatar(avatarUpdate) {
+    api
+      .setUserAvatar(avatarUpdate)
+      .then((newUserData) => {
+        setCurrentUser(newUserData);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleCardLike(card) {
+    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    api
+      .changeLikeCardStatus(card._id, !isLiked)
+      .then((newCard) => {
+        setCards((cards) =>
+          cards.map((c) => (c._id === card._id ? newCard : c)),
+        );
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleCardDelete(card) {
+    api
+      .removeCard(card._id)
+      .then(() => {
+        setCards((cards) => cards.filter((c) => c._id !== card._id));
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleAddPlaceSubmit(newCard) {
+    api
+      .addCard(newCard)
+      .then((newCardFull) => {
+        setCards([newCardFull, ...cards]);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function onRegister({ email, password }) {
+    auth
+      .register(email, password)
+      .then((res) => {
+        setTooltipStatus("success");
+        setIsInfoToolTipOpen(true);
+        history.push("/signin");
+      })
+      .catch((err) => {
+        setTooltipStatus("fail");
+        setIsInfoToolTipOpen(true);
+      });
+  }
+
+  function onLogin({ email, password }) {
+    auth
+      .login(email, password)
+      .then((res) => {
+        setIsLoggedIn(true);
+        setEmail(email);
+        history.push("/");
+      })
+      .catch((err) => {
+        setTooltipStatus("fail");
+        setIsInfoToolTipOpen(true);
+      });
+  }
+
+  function onSignOut() {
+    // при вызове обработчика onSignOut происходит удаление jwt
+    localStorage.removeItem("jwt");
+    setIsLoggedIn(false);
+    // После успешного вызова обработчика onSignOut происходит редирект на /signin
+    history.push("/signin");
+  }
+
+  return (
+    // В компонент App внедрён контекст через CurrentUserContext.Provider
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page__content">
+        <Header email={email} onSignOut={onSignOut} />
+        <Switch>
+          <ProtectedRoute
+            exact
+            path="/"
+            component={Main}
+            cards={cards}
+            onEditProfile={handleEditProfileClick}
+            onAddPlace={handleAddPlaceClick}
+            onEditAvatar={handleEditAvatarClick}
+            onCardClick={handleCardClick}
+            onCardLike={handleCardLike}
+            onCardDelete={handleCardDelete}
+            loggedIn={isLoggedIn}
+          />
+          <Route path="/signup">
+            <Register onRegister={onRegister} />
+          </Route>
+          <Route path="/signin">
+            <Login onLogin={onLogin} />
+          </Route>
+        </Switch>
+        <Footer />
+        <EditProfilePopup
+          isOpen={isEditProfilePopupOpen}
+          onUpdateUser={handleUpdateUser}
+          onClose={closeAllPopups}
+        />
+        <AddPlacePopup
+          isOpen={isAddPlacePopupOpen}
+          onAddPlace={handleAddPlaceSubmit}
+          onClose={closeAllPopups}
+        />
+        <PopupWithForm title="Вы уверены?" name="remove-card" buttonText="Да" />
+        <EditAvatarPopup
+          isOpen={isEditAvatarPopupOpen}
+          onUpdateAvatar={handleUpdateAvatar}
+          onClose={closeAllPopups}
+        />
+        <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+        <InfoTooltip
+          isOpen={isInfoToolTipOpen}
+          onClose={closeAllPopups}
+          status={tooltipStatus}
+        />
+      </div>
+    </CurrentUserContext.Provider>
+  );
+}
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
@@ -11,7 +11,7 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
 
 // If you want your app to work offline and load faster, you can change
