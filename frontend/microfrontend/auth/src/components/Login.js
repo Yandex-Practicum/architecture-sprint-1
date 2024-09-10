07c221dfa6466aec737cb9f99aace0d41c47c406
i.krikunov <i.krikunov@gitlab.tcsbank.ru>
2024-09-10 20:31:59 +0300

    feat(sprint_1_task_1): Перенёс стили для auth

--- frontend/microfrontend/auth/src/components/Login.js
+++ frontend/microfrontend/auth/src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import '../../../../src/blocks/login/login.css';
+import '../blocks/login/login.css';
 
 function Login ({ onLogin }){
   const [email, setEmail] = React.useState('');
