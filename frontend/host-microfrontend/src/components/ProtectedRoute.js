838b10ac89673d7b6bf2b1a2e2514172ea323e2f
r.galeev <r.galeev@finflowsoft.com>
2024-09-20 23:46:17 +0300

    sprint-1

---
+++ frontend/host-microfrontend/src/components/ProtectedRoute.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Redirect } from "react-router-dom";
+
+const ProtectedRoute = ({ component: Component, ...props  }) => {
+  return (
+    <Route exact>
+      {
+        () => props.loggedIn ? <Component {...props} /> : <Redirect to="./signin" />
+      }
+    </Route>
+)}
+
+export default ProtectedRoute;
---
+++ frontend/microfrontend/register/src/utils/auth.js
@@ -0,0 +1,18 @@
+const BASE_URL = 'https://auth.nomoreparties.co';
+
+const getResponse = (res) => {
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+}
+
+export const register = (email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({email, password})
+  })
+  .then(getResponse)
+};
+}
