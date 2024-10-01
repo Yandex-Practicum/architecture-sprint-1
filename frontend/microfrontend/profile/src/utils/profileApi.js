f565054732a2f9f43498ef30e268d01e6be0093d
Mikhail Sorokin <m-sapper@mail.ru>
2024-10-01 14:08:45 +0300

    Add correct routing from main component

---
+++ frontend/microfrontend/profile/src/utils/profileApi.js
@@ -0,0 +1,59 @@
+class ProfileApi {
+  constructor({ address, token, groupId }) {
+    // стандартная реализация -- объект options
+    this._token = token;
+    this._groupId = groupId;
+    this._address = address;
+
+    // Запросы в примере работы выполняются к старому Api, в новом URL изменены.
+  }
+
+  getUserInfo() {
+    return fetch(`${this._address}/${this._groupId}/users/me`, {
+      headers: {
+        authorization: this._token,
+      },
+    }).then((res) =>
+      res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
+    );
+  }
+
+  setUserInfo({ name, about }) {
+    return fetch(`${this._address}/${this._groupId}/users/me`, {
+      method: "PATCH",
+      headers: {
+        authorization: this._token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    }).then((res) =>
+      res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
+    );
+  }
+
+  setUserAvatar({ avatar }) {
+    return fetch(`${this._address}/${this._groupId}/users/me/avatar`, {
+      method: "PATCH",
+      headers: {
+        authorization: this._token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        avatar,
+      }),
+    }).then((res) =>
+      res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
+    );
+  }
+}
+
+const profileApi = new ProfileApi({
+  address: "https://nomoreparties.co",
+  groupId: `cohort0`,
+  token: `80a75492-21c5-4330-a02f-308029e94b63`,
+});
+
+export default profileApi;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ["@svgr/webpack", "url-loader"],
+      },
+    ],
+  },
+  entry: "./src/index",
+  output: {
+    publicPath: "auto",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        auth: "auth@http://localhost:3001/remoteEntry.js",
+        profile: "profile@http://localhost:3002/remoteEntry.js",
+        post: "post@http://localhost:3003/remoteEntry.js",
+      },
+      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
